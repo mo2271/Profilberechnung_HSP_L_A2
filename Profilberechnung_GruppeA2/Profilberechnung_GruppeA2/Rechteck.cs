b5f3f1ad9b1e68b223f38b694c4390ab41c682f9
mo2271 <moritz.schwartz@student.jade-hs.de>
2020-04-27 15:12:22 +0200

    Update Rechteck.cs

--- Profilberechnung_GruppeA2/Profilberechnung_GruppeA2/Rechteck.cs
+++ Profilberechnung_GruppeA2/Profilberechnung_GruppeA2/Rechteck.cs
@@ -17,20 +17,7 @@ namespace Profilberechnung_GruppeA2
             Breite_Rechteck = local_Breite;
             Hoehe_Rechteck = local_Hoehe;
             Laenge_Rechteck = local_Laenge;
-            return Breite_Rechteck + Hoehe_Rechteck + Laenge_Rechteck;
-            
+            return Breite_Rechteck + Hoehe_Rechteck + Laenge_Rechteck;    
         }
-
-        //public double setBreite(double local_Breite)
-        //{
-        //    Breite_Rechteck = local_Breite;
-        //    return Breite_Rechteck;
-        //}
-
-        //public double setHoehe(double local_Hoehe)
-        //{
-        //    Hoehe_Rechteck = local_Hoehe;
-        //    return Hoehe_Rechteck;
-        //}
     }
 }
