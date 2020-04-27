5b6ae3deb267d4a38e27ec74d2384feb54c5fcc6
mo2271 <moritz.schwartz@student.jade-hs.de>
2020-04-27 15:27:53 +0200

    Update Program.cs

--- Profilberechnung_GruppeA2/Profilberechnung_GruppeA2/Program.cs
+++ Profilberechnung_GruppeA2/Profilberechnung_GruppeA2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,9 @@ namespace Profilberechnung_GruppeA2
             double Hoehe;
             double Laenge;
             double Flaeche, Volumen;
+            string Masseinheit;
+
+            Console.WriteLine("Berechnung eines Rechteckprofils gestartet.");
             Console.WriteLine("Bitte geben Sie die Breite an:");
             Breite = Convert.ToDouble(Console.ReadLine());
 
@@ -22,6 +26,11 @@ namespace Profilberechnung_GruppeA2
 
             Console.WriteLine("Bitte geben Sie die Länge des Profilstrangs an:");
             Laenge = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Bitte geben Sie die verwendete Masseinheit an:");
+            Masseinheit = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Profil-Kennwerte werden berechnet...");
             
             Rechteck R = new Rechteck();
             R.setGeometrie(Breite, Hoehe, Laenge);
@@ -30,20 +39,16 @@ namespace Profilberechnung_GruppeA2
             Flaeche = R.Breite_Rechteck * R.Hoehe_Rechteck;
             Volumen = R.Breite_Rechteck * R.Hoehe_Rechteck * R.Laenge_Rechteck;
 
-            Console.WriteLine("Flächeninhalt: " + Flaeche);
-            Console.WriteLine("Volumen: " + Volumen);
-            Console.ReadKey();
+            Console.WriteLine("Flächeninhalt: " + Flaeche + Masseinheit + "^2");
+            Console.WriteLine("Volumen: " + Volumen + Masseinheit + "^3");
         }
         static void Main(string[] args)
         {
-            double Breite;
-            double Hoehe;
-            double Laenge;
+            
             string Werkstoff;
             double Dichte;
 
-            double Flaeche;
-            double Volumen;
+            
             double Gewicht;
             double FTM_X;
             double FTM_Y;
@@ -59,9 +64,9 @@ namespace Profilberechnung_GruppeA2
             }
             else
             {
-                Console.WriteLine("... noch nicht implementiert...");
+                Console.WriteLine("... noch nicht implementiert ...");
             }
-
+            Console.WriteLine("Beliebige Taste zum Beenden drücken...");
             Console.ReadKey();
         }
     }
