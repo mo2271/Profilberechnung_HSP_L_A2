6d1424095dba2a14f1b3b717aa8a31f1314300ec
mo2271 <moritz.schwartz@student.jade-hs.de>
2020-04-27 14:27:41 +0200

    Update Program.cs

--- Profilberechnung_GruppeA2/Profilberechnung_GruppeA2/Program.cs
+++ Profilberechnung_GruppeA2/Profilberechnung_GruppeA2/Program.cs
@@ -8,6 +8,12 @@ namespace Profilberechnung_GruppeA2
 {
     class Program
     {
+        static void Rechteckprofil()
+        {
+            Rechteck R = new Rechteck();
+
+
+        }
         static void Main(string[] args)
         {
             float Breite;
@@ -21,6 +27,16 @@ namespace Profilberechnung_GruppeA2
             float Gewicht;
             float FTM_X;
             float FTM_Y;
+
+            string Eingabe;
+
+            Console.WriteLine("Bitte geben Sie den zu berechnenden Profiltyp an: [1] Rechteck-Profil; [2]...");
+
+            Eingabe = Convert.ToString(Console.ReadLine());
+            if (Eingabe.Equals(1))
+            {
+                Rechteckprofil();
+            }
         }
     }
 }
