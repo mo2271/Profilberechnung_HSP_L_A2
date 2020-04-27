b1eef29605acc4f4df9019db3b43f91a6580b117
mo2271 <moritz.schwartz@student.jade-hs.de>
2020-04-27 15:03:34 +0200

    Update Program.cs

--- Profilberechnung_GruppeA2/Profilberechnung_GruppeA2/Program.cs
+++ Profilberechnung_GruppeA2/Profilberechnung_GruppeA2/Program.cs
@@ -10,33 +10,59 @@ namespace Profilberechnung_GruppeA2
     {
         static void Rechteckprofil()
         {
+            double Breite;
+            double Hoehe;
+            double Laenge;
+            double Flaeche, Volumen;
+            Console.WriteLine("Bitte geben Sie die Breite an:");
+            Breite = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Bitte geben Sie die Hoehe an:");
+            Hoehe = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Bitte geben Sie die Länge des Profilstrangs an:");
+            Laenge = Convert.ToDouble(Console.ReadLine());
+            
             Rechteck R = new Rechteck();
+            R.setGeometrie(Breite, Hoehe, Laenge);
+            
 
+            Flaeche = R.Breite_Rechteck * R.Hoehe_Rechteck;
+            Volumen = R.Breite_Rechteck * R.Hoehe_Rechteck * R.Laenge_Rechteck;
 
+            Console.WriteLine("Flächeninhalt: " + Flaeche);
+            Console.WriteLine("Volumen: " + Volumen);
+            Console.ReadKey();
         }
         static void Main(string[] args)
         {
-            float Breite;
-            float Hoehe;
-            float Laenge;
+            double Breite;
+            double Hoehe;
+            double Laenge;
             string Werkstoff;
             double Dichte;
 
-            float Flaeche;
-            float Volumen;
-            float Gewicht;
-            float FTM_X;
-            float FTM_Y;
+            double Flaeche;
+            double Volumen;
+            double Gewicht;
+            double FTM_X;
+            double FTM_Y;
 
             string Eingabe;
 
             Console.WriteLine("Bitte geben Sie den zu berechnenden Profiltyp an: [1] Rechteck-Profil; [2]...");
 
             Eingabe = Convert.ToString(Console.ReadLine());
-            if (Eingabe.Equals(1))
+            if (Eingabe.Equals("1"))
             {
                 Rechteckprofil();
             }
+            else
+            {
+                Console.WriteLine("... noch nicht implementiert...");
+            }
+
+            Console.ReadKey();
         }
     }
 }
