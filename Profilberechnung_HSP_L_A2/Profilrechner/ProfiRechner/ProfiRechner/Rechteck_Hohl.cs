1e495004271274cb7daf04c4de6f959fe4404e0f
Al2858 <61633578+Al2858@users.noreply.github.com>
2020-05-14 21:32:36 +0200

    Berechnungen ohne Masse, Einheiten, Laenge

---
+++ Profilberechnung_HSP_L_A2/Profilrechner/ProfiRechner/ProfiRechner/Rechteck_Hohl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfiRechner
+{
+    class Rechteck_Hohl
+    {
+        public double KlasseRechteckHohlBreite;
+        public double KlasseRechteckHohlHoehe;
+        public double KlasseRechteckHohlLaenge;
+        public double KlasseRechteckHohlWandstaerke;
+
+        public double setGeometrie(double local_RechteckHohlBreite, double local_RechteckHohlHoehe, double local_RechteckHohlLaenge, double local_RechteckHohlWandstaerke)
+        {
+            KlasseRechteckHohlBreite = local_RechteckHohlBreite;
+            KlasseRechteckHohlHoehe = local_RechteckHohlHoehe;
+            KlasseRechteckHohlLaenge = local_RechteckHohlLaenge;
+            KlasseRechteckHohlWandstaerke = local_RechteckHohlWandstaerke;
+            return KlasseRechteckHohlBreite + KlasseRechteckHohlHoehe + KlasseRechteckHohlLaenge + KlasseRechteckHohlWandstaerke;
+        }
+    }
+}
