858cb866264ab91ef3372da776215b85aace2cb1
mo2271 <moritz.schwartz@student.jade-hs.de>
2020-05-29 12:20:10 +0200

    Create CATIAConnection.cs

---
+++ Profilberechnung_HSP_L_A2/Profilrechner/ProfiRechner/ProfiRechner/CATIAConnection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using INFITF;
+using MECMOD;
+using PARTITF;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ProfiRechner
+{
+    class CATIAConnection
+    {
+        INFITF.Application CATIA_ProfilProfi;
+        
+        public bool CATIA_Run()
+        {
+            try
+            {
+                object catiaObject = System.Runtime.InteropServices.Marshal.GetActiveObject("CATIA.Application");
+                CATIA_ProfilProfi = (INFITF.Application)catiaObject;
+
+                
+
+                return true;
+            }
+            catch (Exception)
+            {
+                
+
+                return false;
+            }
+        }
+    }
+}
